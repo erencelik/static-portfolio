34146a9ad8cbaee9c76b43245852eeccb7ebef8e
erencelik <eren.celik@icloud.com>
2021-09-29 01:31:42 +0300

    Linux ubuntu console mimic added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 let titles = [
+    "iOS Dev3loper",
+    "iOS D3veloper",
+    "iOS Develop3r",
+    "i0S Developer",
+    "iOX Developer",
+    "iOS Develo?er",
+    "iOS Deve?oper",
+    "1os Developer",
+    "iO5 Developer",
+    "i?S Developer",
+    "iOS @eveloper",
+    "i@s Developer",
+    "iOS Deve1oper",
+    "iOS Develope?",
+    "iOS ?eveloper",
+    "iO? Developer",
+    "!os Developer",
+    "iOS Deve!oper",
+    "?OS Developer",
+    "iOS De?eloper",
+    "iOS D?veloper"
+];
+
+let _titles = [
     "@root",
     "whoami",
     "ind1e",
@@ -26,39 +50,80 @@ let titles = [
     "error"
 ];
 
-let colors = [
-    "red",
-    "green",
-    "yellowgreen", 
-    "cyan", 
-    "teal", 
-    "turquoise",
-    "orange",
-    "gold",
-    "yellow"
-];
-
-var initialTitle;
-
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 document.addEventListener("DOMContentLoaded", function(event) {
-
-    initialTitle = document.getElementById("switcher").innerHTML;
 
     setInterval(() => {
 
         document.title = document.title == "erencelik" ? "0x65726e" : "erencelik";
 
-        let count = getRandomInt(3, 9);
+        consoleIterateTyping();
 
-        let interval = getRandomInt(66, 99);
-
-        loopTitles(count, interval);
-
-    }, 6480);
+    }, 6000);
 
 });
+
+function consoleIterateTyping() {
+
+    let element = document.getElementById("console-input");
+
+    element.innerHTML = "";
+
+    if(!element.classList.contains("typing")) {
+        element.classList.add("typing");
+    }
+
+    document.getElementById("switcher").innerHTML = "&nbsp;";
+
+    setTimeout(() => {
+        consoleMimicTypeWrite();
+    }, 360);
+
+}
+
+var inputIndex = 0;
+var inputText = 'whoami';
+
+function consoleMimicTypeWrite() {
+
+    let element = document.getElementById("console-input");
+
+    if (inputIndex < inputText.length) {
+
+        element.innerHTML += inputText.charAt(inputIndex);
+
+        inputIndex++;
+
+        setTimeout(consoleMimicTypeWrite, 128);
+
+    } else {
+
+        if(element.innerHTML == inputText) {
+
+            inputIndex = 0;
+
+            setTimeout(() => {
+                
+                element.classList.remove("typing");
+
+                setTimeout(() => {
+
+                    let count = getRandomInt(4, 7);
+
+                    let interval = getRandomInt(77, 99);
+
+                    loopTitles(count, interval);
+
+                }, 360);
+                
+            }, 360);
+
+        }
+
+    }
+
+}
 
 function loopTitles(count, interval) {
 
@@ -80,7 +145,7 @@ function loopTitles(count, interval) {
 
             if(i == count) {
 
-                element.innerHTML = initialTitle;
+                element.innerHTML = "iOS Developer";
 
             } else {
 
@@ -104,8 +169,4 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
-}
-
-function getRandomColor() {
-    return colors[getRandomInt(0, colors.length)];
 }
